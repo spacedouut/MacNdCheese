24958c3818ab89b04778d905657b84ae809e4e0d
Cheesy porting <84504416+mont127@users.noreply.github.com>
2026-03-28 12:55:33 +0100

    Bump version from v5.1.0 to v5.1.2

--- MacNdCheeseARM.py
+++ MacNdCheeseARM.py
@@ -893,7 +893,7 @@ QGroupBox::title {
 """
 
 APP_NAME = "MacNCheese"
-APP_VERSION = "v5.1.0"
+APP_VERSION = "v5.1.2"
 GITHUB_REPO = "mont127/MacNdCheese"
 GITHUB_LATEST_RELEASE_API = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
